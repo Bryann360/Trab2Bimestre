ec7728e655045e5d27b37756f6cc5ea3c4e288e1
João Victor Bonfim Tavares Miguel <katerana2@gmail.com>
2018-08-05 20:24:04 -0300

    Percebi que existia já um ImplicareServlet.java, removi o meu ServletImpl.java do projeto e adicionei modificações menores no código anteriormente feito pelo GB, também Adicionei uma função execute para PesquisarCidade.java que servirá como uma fução temporária, só para não dar erros de compilação. Considerando modificar os HTMLs das páginas para torná-las "rodáveis".

--- Implicare/src/Source Packages/br/cefetmg/implicare/servlet/ImplicareServlet.java
+++ Implicare/src/Source Packages/br/cefetmg/implicare/servlet/ImplicareServlet.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.cefetmg.implicare.servlet;
 
+import br.cefetmg.implicare.servlet.Exceptions.InvalidRequestException;
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -16,179 +12,188 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Gabriel
  */
-public class ImplicareServlet extends HttpServlet {
-
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        //Declarando Variaveis
-        String jsp = "";
-        String acao = request.getParameter("acao");
-        // Encode Dos Caracteres
+public class ImplicareServlet extends HttpServlet
+{
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        //Defini,c~ao da decodifica,c~ao da solicita,c~ao.
         request.setCharacterEncoding("UTF-8");
-
-        //Redirecionando Para Classe de Serviço
-        if (null != acao) {
-            switch (acao) {
-                case "AlterarCandidatoVaga":
-                    jsp = AlterarCandidatoVaga.execute(request);
-                    break;
-                case "AlterarEmpresa":
-                    jsp = AlterarEmpresa.execute(request);
-                    break;
-                case "AlterarExperienciaProfissional":
-                    jsp = AlterarExperienciaProfissional.execute(request);
-                    break;
-                case "AlterarFormacaoAcademica":
-                    jsp = AlterarFormacaoAcademica.execute(request);
-                    break;
-                case "AlterarPessoaFisica":
-                    jsp = AlterarPessoaFisica.execute(request);
-                    break;
-                case "AlterarTelefone":
-                    jsp = AlterarTelefone.execute(request);
-                    break;
-                case "AlterarUsuario":
-                    jsp = AlterarUsuario.execute(request);
-                    break;
-                case "AlterarVaga":
-                    jsp = AlterarVaga.execute(request);
-                    break;
-                case "ExcluirCandidatoVaga":
-                    jsp = ExcluirCandidatoVaga.execute(request);
-                    break;
-                case "ExcluirCargoInteresse":
-                    jsp = ExcluirCargoInteresse.execute(request);
-                    break;
-                case "ExcluirCompetenciaPessoaFisica":
-                    jsp = ExcluirCompetenciaPessoaFisica.execute(request);
-                    break;
-                case "ExcluirExperienciaProfissional":
-                    jsp = ExcluirExperienciaProfissional.execute(request);
-                    break;
-                case "ExcluirFormacaoAcademica":
-                    jsp = ExcluirFormacaoAcademica.execute(request);
-                    break;
-                case "ExcluirTelefone":
-                    jsp = ExcluirTelefone.execute(request);
-                    break;
-                case "ExcluirVaga":
-                    jsp = ExcluirVaga.execute(request);
-                    break;
-                case "InserirCandidatoVaga":
-                    jsp = InserirCandidatoVaga.execute(request);
-                    break;
-                case "InserirCandidatoVagaDialogo":
-                    jsp = InserirCandidatoVagaDialogo.execute(request);
-                    break;
-                case "InserirCargoInteresse":
-                    jsp = InserirCargoInteresse.execute(request);
-                    break;
-                case "InserirCompetenciaPessoaFisica":
-                    jsp = InserirCompetenciaPessoaFisica.execute(request);
-                    break;
-                case "InserirEmpresa":
-                    jsp = InserirEmpresa.execute(request);
-                    break;
-                case "InserirExperienciaProfissional":
-                    jsp = InserirExperienciaProfissional.execute(request);
-                    break;
-                case "InserirFormacaoAcademica":
-                    jsp = InserirFormacaoAcademica.execute(request);
-                    break;
-                case "InserirPessoaFisica":
-                    jsp = InserirPessoaFisica.execute(request);
-                    break;
-                case "InserirTelefone":
-                    jsp = InserirTelefone.execute(request);
-                    break;
-                case "InserirUsuario":
-                    jsp = InserirUsuario.execute(request);
-                    break;
-                case "InserirVaga":
-                    jsp = InserirVaga.execute(request);
-                    break;
-                case "ListarAreaEstudo":
-                    jsp = ListarAreaEstudo.execute(request);
-                    break;
-                case "ListarCandidatoVaga":
-                    jsp = ListarCandidatoVaga.execute(request);
-                    break;
-                case "ListarCandidatoVagaDialogo":
-                    jsp = ListarCandidatoVagaDialogo.execute(request);
-                    break;
-                case "ListarCargo":
-                    jsp = ListarCargo.execute(request);
-                    break;
-                case "ListarCargoInteresse":
-                    jsp = ListarCargoInteresse.execute(request);
-                    break;
-                case "ListarCargoPessoaFisica":
-                    jsp = ListarCargoPessoaFisica.execute(request);
-                    break;
-                case "ListarCidade":
-                    jsp = ListarCidade.execute(request);
-                    break;
-                case "ListarCompetencia":
-                    jsp = ListarCompetencia.execute(request);
-                    break;
-                case "ListarEstado":
-                    jsp = ListarEstado.execute(request);
-                    break;
-                case "ListarExperienciaProfissional":
-                    jsp = ListarExperienciaProfissional.execute(request);
-                    break;
-                case "ListarFormacaoAcademica":
-                    jsp = ListarFormacaoAcademica.execute(request);
-                    break;
-                case "ListarProficiencia":
-                    jsp = ListarProficiencia.execute(request);
-                    break;
-                case "ListarTelefone":
-                    jsp = ListarTelefone.execute(request);
-                    break;
-                case "ListarVaga":
-                    jsp = ListarVaga.execute(request);
-                    break;
-                case "ListarVagaPessoaFisica":
-                    jsp = ListarVagaPessoaFisica.execute(request);
-                    break;
-                case "Logar":
-                    jsp = Login.execute(request);
-                    break;
-                case "PesquisarAreaEstudo":
-                    jsp = PesquisarAreaEstudo.execute(request);
-                    break;
-                case "PesquisarCargo":
-                    jsp = PesquisarCargo.execute(request);
-                    break;
-                case "PesquisarCompetencia":
-                    jsp = PesquisarCompetencia.execute(request);
-                    break;
-                case "PesquisarEmpresa":
-                    jsp = PesquisarEmpresa.execute(request);
-                    break;
-                case "PesquisarEstado":
-                    jsp = PesquisarEstado.execute(request);
-                    break;
-                case "PesquisarPessoaFisica":
-                    jsp = PesquisarPessoaFisica.execute(request);
-                    break;
-                case "PesquisarUsuario":
-                    jsp = PesquisarUsuario.execute(request);
-                    break;
-                case "PesquisarProficiencia":
-                    jsp= PesquisarProficiencia.execute(request);
-                    break;
-                default:
-                    jsp = "/WEB-Pages/Erro.jsp";
-                    request.setAttribute("Erro", "Erro Geral");
-                    break;
-            }
+        
+        //Declara,c~ao de vari´aveis.
+        String jsp;
+        String action = request.getParameter("action");
+        
+        //Testa se a solicitação ´e vazia ou nula.
+        if(action == null || action.isEmpty())
+        {throw new InvalidRequestException("Erro: nenhuma ação foi solicitada.");}
+        
+        /*
+            Testa se a solicita,c~ao ´e v´alida, se ´e executa a solicit,c~ao,
+            se n~ao redireciona para uma p´agina de erro.
+        */
+        switch (action) {
+            case "AlterarCandidatoVaga":
+                jsp = AlterarCandidatoVaga.execute(request);
+                break;
+            case "AlterarEmpresa":
+                jsp = AlterarEmpresa.execute(request);
+                break;
+            case "AlterarExperienciaProfissional":
+                jsp = AlterarExperienciaProfissional.execute(request);
+                break;
+            case "AlterarFormacaoAcademica":
+                jsp = AlterarFormacaoAcademica.execute(request);
+                break;
+            case "AlterarPessoaFisica":
+                jsp = AlterarPessoaFisica.execute(request);
+                break;
+            case "AlterarTelefone":
+                jsp = AlterarTelefone.execute(request);
+                break;
+            case "AlterarUsuario":
+                jsp = AlterarUsuario.execute(request);
+                break;
+            case "AlterarVaga":
+                jsp = AlterarVaga.execute(request);
+                break;
+            case "ExcluirCandidatoVaga":
+                jsp = ExcluirCandidatoVaga.execute(request);
+                break;
+            case "ExcluirCargoInteresse":
+                jsp = ExcluirCargoInteresse.execute(request);
+                break;
+            case "ExcluirCompetenciaPessoaFisica":
+                jsp = ExcluirCompetenciaPessoaFisica.execute(request);
+                break;
+            case "ExcluirExperienciaProfissional":
+                jsp = ExcluirExperienciaProfissional.execute(request);
+                break;
+            case "ExcluirFormacaoAcademica":
+                jsp = ExcluirFormacaoAcademica.execute(request);
+                break;
+            case "ExcluirTelefone":
+                jsp = ExcluirTelefone.execute(request);
+                break;
+            case "ExcluirVaga":
+                jsp = ExcluirVaga.execute(request);
+                break;
+            case "InserirCandidatoVaga":
+                jsp = InserirCandidatoVaga.execute(request);
+                break;
+            case "InserirCandidatoVagaDialogo":
+                jsp = InserirCandidatoVagaDialogo.execute(request);
+                break;
+            case "InserirCargoInteresse":
+                jsp = InserirCargoInteresse.execute(request);
+                break;
+            case "InserirCompetenciaPessoaFisica":
+                jsp = InserirCompetenciaPessoaFisica.execute(request);
+                break;
+            case "InserirEmpresa":
+                jsp = InserirEmpresa.execute(request);
+                break;
+            case "InserirExperienciaProfissional":
+                jsp = InserirExperienciaProfissional.execute(request);
+                break;
+            case "InserirFormacaoAcademica":
+                jsp = InserirFormacaoAcademica.execute(request);
+                break;
+            case "InserirPessoaFisica":
+                jsp = InserirPessoaFisica.execute(request);
+                break;
+            case "InserirTelefone":
+                jsp = InserirTelefone.execute(request);
+                break;
+            case "InserirUsuario":
+                jsp = InserirUsuario.execute(request);
+                break;
+            case "InserirVaga":
+                jsp = InserirVaga.execute(request);
+                break;
+            case "ListarAreaEstudo":
+                jsp = ListarAreaEstudo.execute(request);
+                break;
+            case "ListarCandidatoVaga":
+                jsp = ListarCandidatoVaga.execute(request);
+                break;
+            case "ListarCandidatoVagaDialogo":
+                jsp = ListarCandidatoVagaDialogo.execute(request);
+                break;
+            case "ListarCargo":
+                jsp = ListarCargo.execute(request);
+                break;
+            case "ListarCargoInteresse":
+                jsp = ListarCargoInteresse.execute(request);
+                break;
+            case "ListarCargoPessoaFisica":
+                jsp = ListarCargoPessoaFisica.execute(request);
+                break;
+            case "ListarCidade":
+                jsp = ListarCidade.execute(request);
+                break;
+            case "ListarCompetencia":
+                jsp = ListarCompetencia.execute(request);
+                break;
+            case "ListarEstado":
+                jsp = ListarEstado.execute(request);
+                break;
+            case "ListarExperienciaProfissional":
+                jsp = ListarExperienciaProfissional.execute(request);
+                break;
+            case "ListarFormacaoAcademica":
+                jsp = ListarFormacaoAcademica.execute(request);
+                break;
+            case "ListarProficiencia":
+                jsp = ListarProficiencia.execute(request);
+                break;
+            case "ListarTelefone":
+                jsp = ListarTelefone.execute(request);
+                break;
+            case "ListarVaga":
+                jsp = ListarVaga.execute(request);
+                break;
+            case "ListarVagaPessoaFisica":
+                jsp = ListarVagaPessoaFisica.execute(request);
+                break;
+            case "Login":
+                jsp = Login.execute(request);
+                break;
+            case "PesquisarAreaEstudo":
+                jsp = PesquisarAreaEstudo.execute(request);
+                break;
+            case "PesquisarCargo":
+                jsp = PesquisarCargo.execute(request);
+                break;
+            case "PesquisarCidade":
+                jsp = PesquisarCidade.execute(request);
+                break;
+            case "PesquisarCompetencia":
+                jsp = PesquisarCompetencia.execute(request);
+                break;
+            case "PesquisarEmpresa":
+                jsp = PesquisarEmpresa.execute(request);
+                break;
+            case "PesquisarEstado":
+                jsp = PesquisarEstado.execute(request);
+                break;
+            case "PesquisarPessoaFisica":
+                jsp = PesquisarPessoaFisica.execute(request);
+                break;
+            case "PesquisarProficiencia":
+                jsp = PesquisarProficiencia.execute(request);
+                break;
+            case "PesquisarUsuario":
+                jsp = PesquisarUsuario.execute(request);
+                break;
+            default:
+                jsp = "/WEB-Pages/Erro.jsp";
+                request.setAttribute("Erro", "A solicitação feita ao servlet é inválida ou ainda não foi implementada.");
         }
-        RequestDispatcher rd = request.getRequestDispatcher(jsp);
-
-        //Redirecioando Pra Uma Nova Pagina
-        rd.forward(request, response);
+        
+        //Cria um dispatcher da solicita,c~ao.
+        RequestDispatcher dispatcher = request.getRequestDispatcher(jsp);
+        //Redireciona a p´agina.
+        dispatcher.forward(request, response);
     }
-
 }
